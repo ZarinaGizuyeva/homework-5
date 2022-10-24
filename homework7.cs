b8b6d48e172a2a116720045eb3db3cde0544fa46
Zarina <giueva.zarina@gmail.com>
2022-10-24 10:10:26 +0600

    homework 8 sem

--- homework7.cs
+++ homework7.cs
@@ -1,101 +1,54 @@
-// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-
+// Задача 53:**Задайте двумерный массив.Напишите программу, которая поменяет местами первую и последнюю строку массива.
 // Console.Clear();
-// void InputMatrix(double[,] matrix)
-// {
-// for (int i = 0; i < matrix.GetLength(0); i++)
-// {
-// for (int j = 0; j < matrix.GetLength(1); j++)
-// {
-// matrix[i, j] = Math.Round(new Random().NextDouble() * 10, 3);  
-// Console.Write(matrix[i, j] + " \t");
-// }
+
+// Console.Write("Введите количество строк массива: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// int[,] array = GetArray(rows, columns, 0, 10);
+// PrintArray(array);
 // Console.WriteLine();
-// }
-// }
+// ChangeRows(array);
+// PrintArray(array);
 
-// Console.Write("Введите кол-во строк: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите кол-во столбцов: ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// double[,] matrix = new double[n, m];
-// InputMatrix(matrix);
-
-
-// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-
-// Console.Clear();
-
-// Console.Write("Введите кол-во строк: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите кол-во столбцов: ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// int[,] matrix = new int[n, m];
-
-// void InputMatrix(int[,] matrix)
+// int[,] GetArray(int m, int n, int min, int max)
 // {
-//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
 //     {
-//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         for (int j = 0; j < n; j++)
 //         {
-//             matrix[i, j] = new Random().Next(1, 15);
-//             Console.Write(matrix[i, j] + " \t");
+//             result[i, j] = new Random().Next(min, max + 1);
+//         }
+
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]} ");
 //         }
 //         Console.WriteLine();
 //     }
+// }
 
-//     double[] summ = new double[matrix.GetLength(1)];
-//     for (int i = 0; i < matrix.GetLength(0); i++)
+// void ChangeRows(int[,] inArray)
+// {
+//     int rowCount = inArray.GetLength(0) - 1;
+//     for (int i = 0; i < inArray.GetLength(1); i++)
 //     {
-//         for (int j = 0; j < matrix.GetLength(1); j++)
-//         {
-//             summ[i] += matrix[j, i];      //подсчет суммы колонки
-//         }
-//         Console.Write($"Среднее арифметическое столбцов: " + summ[i] / matrix.GetLength(1)); //вывод среднего для колонки
+//         int k = inArray[0, i];
+//         inArray[0, i] = inArray[rowCount, i];
+//         inArray[rowCount, i] = k;
 //     }
 // }
 
-// InputMatrix(matrix);
 
 
-// Задача 50.Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-
-Console.Clear();
-
-Console.Write("Введите кол-во строк: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кол-во столбцов: ");
-int m = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = new int[n, m];
-
-void InputMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = new Random().Next(1, 15);
-            Console.Write(matrix[i, j] + " \t");
-        }
-        Console.WriteLine();
-    }
-}
-InputMatrix(matrix);
-Console.Write("Введите позицию строки: ");
-int line = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите позицию столбца: ");
-int column = Convert.ToInt32(Console.ReadLine());
-
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-        if (i + 1 == line && j + 1 == column)
-            Console.WriteLine(matrix[i, j]);
-        else if (line > matrix.GetLength(0) && column > matrix.GetLength(1))
-            Console.WriteLine("Такого элемента нет");
-    }
-
-}
 
 
